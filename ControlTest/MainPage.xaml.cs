d4ccb8c660e49117f460e78b4850efa3060162b1
Mia Widstrand <mia.widstrand@live.se>
2015-12-07 15:42:22 +0100

    first commit

---
+++ ControlTest/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ControlTest
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Clicked(object sender, RoutedEventArgs e)
+        {
+            ToolTempButton.Content = " Tool Temp is 203";
+        }
+
+        private void CheckSelection(object sender, SelectionChangedEventArgs e)
+        {
+            ContentDialog CheckAnswer = new ContentDialog()
+            {
+                Title = "No wifi connection",
+                Content = "Check connection and try again",
+                PrimaryButtonText = "Ok"
+            };
+           //must find out how to show the dialog box
+        }
+    }
+}
